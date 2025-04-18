a1c7da1f760c2e6d29b198e7ce5f9b71eaec81a1
Vasiliev Victor <vasilievvict1@yandex.ru>
2025-04-18 19:32:33 +0300

    Added common swagger support in gateway

--- crosswave-gateway/src/main/kotlin/com/xxavierr404/crosswave/gateway/configuration/GatewayConfiguration.kt
+++ crosswave-gateway/src/main/kotlin/com/xxavierr404/crosswave/gateway/configuration/GatewayConfiguration.kt
@@ -5,6 +5,7 @@ import com.xxavierr404.crosswave.gateway.filters.EnrichWithUserDataFilter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory.NameConfig
 import org.springframework.cloud.gateway.filter.factory.RemoveRequestHeaderGatewayFilterFactory
+import org.springframework.cloud.gateway.filter.factory.StripPrefixGatewayFilterFactory
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
@@ -23,6 +24,27 @@ class GatewayConfiguration {
         builder: RouteLocatorBuilder,
         enrichWithUserDataFilter: EnrichWithUserDataFilter,
     ): RouteLocator = builder.routes()
+        .route("auth-swagger") { it ->
+            val config = StripPrefixGatewayFilterFactory.Config()
+            config.parts = 1
+            it.path("/auth/v3/api-docs").filters {
+                it.filter(StripPrefixGatewayFilterFactory().apply(config))
+            }.uri("lb://crosswave-auth")
+        }
+        .route("music-swagger") { it ->
+            val config = StripPrefixGatewayFilterFactory.Config()
+            config.parts = 1
+            it.path("/music/v3/api-docs").filters {
+                it.filter(StripPrefixGatewayFilterFactory().apply(config))
+            }.uri("lb://crosswave-music")
+        }
+        .route("profiles-swagger") { it ->
+            val config = StripPrefixGatewayFilterFactory.Config()
+            config.parts = 1
+            it.path("/profile/v3/api-docs").filters {
+                it.filter(StripPrefixGatewayFilterFactory().apply(config))
+            }.uri("lb://crosswave-analytics")
+        }
         .route("auth") {
             it.path("/auth/**").uri("lb://crosswave-auth")
         }
