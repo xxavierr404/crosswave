2ec3bd1cb6f2ea32344647d3266898f599a179bf
Vasiliev Victor <vasilievvict1@yandex.ru>
2025-04-19 03:22:55 +0300

    Added common swagger support in gateway

--- crosswave-analytics/src/main/kotlin/com/xxavierr404/crosswave/analytics/CrosswaveAnalyticsApplication.kt
+++ crosswave-analytics/src/main/kotlin/com/xxavierr404/crosswave/analytics/CrosswaveAnalyticsApplication.kt
@@ -1,9 +1,15 @@
 package com.xxavierr404.crosswave.analytics
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
+@OpenAPIDefinition(
+	servers = [
+		Server(url = "/", description = "Default server")
+	]
+)
 @SpringBootApplication
 class CrosswaveAnalyticsApplication
 
--- crosswave-auth/src/main/kotlin/com/xxavierr404/crosswave/auth/CrosswaveApplication.kt
+++ crosswave-auth/src/main/kotlin/com/xxavierr404/crosswave/auth/CrosswaveApplication.kt
@@ -1,9 +1,16 @@
 package com.xxavierr404.crosswave.auth
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+	servers = [
+		Server(url = "/", description = "Default server")
+	]
+)
 @SpringBootApplication
 @EnableConfigurationProperties
 class CrosswaveApplication
